4c29edb175035cd05304a005476a70597418bbd2
Shubham16598 <shubham16598@gmail.com>
2017-12-28 08:55:07 +0530

    week 5 part 1 completed

--- src/spelling/DictionaryBST.java
+++ src/spelling/DictionaryBST.java
@@ -12,8 +12,10 @@ public class DictionaryBST implements Dictionary
 	
     // TODO: Implement the dictionary interface using a TreeSet.  
  	// You'll need a constructor here
-	
-    
+    public DictionaryBST(){
+        dict = new TreeSet<String>();
+    }
+
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
      * @param word The word to add
@@ -21,7 +23,14 @@ public class DictionaryBST implements Dictionary
      * (it wasn't already there). */
     public boolean addWord(String word) {
     	// TODO: Implement this method
-        return false;
+        String wordAdd = word.toLowerCase();
+        if (dict.contains(wordAdd)){
+            return false;
+        }
+        else{
+            dict.add(wordAdd);
+            return true;
+        }
     }
 
 
@@ -29,13 +38,19 @@ public class DictionaryBST implements Dictionary
     public int size()
     {
     	// TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
     	//TODO: Implement this method
-        return false;
+        String stringCheck = s.toLowerCase();
+        if (dict.contains(stringCheck)){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
 }
--- src/spelling/DictionaryLL.java
+++ src/spelling/DictionaryLL.java
@@ -13,30 +13,42 @@ public class DictionaryLL implements Dictionary
 	
     // TODO: Add a constructor
 
-
+    public DictionaryLL(){
+        dict = new LinkedList<String>();
+    }
     /** Add this word to the dictionary.  Convert it to lowercase first
      * for the assignment requirements.
      * @param word The word to add
      * @return true if the word was added to the dictionary 
      * (it wasn't already there). */
     public boolean addWord(String word) {
-    	// TODO: Implement this method
-        return false;
+        String wordAdd = word.toLowerCase();
+        if (dict.contains(wordAdd)){
+            return false;
+        }
+        else{
+            dict.add(wordAdd);
+            return true;
+        }
     }
 
 
     /** Return the number of words in the dictionary */
     public int size()
     {
-        // TODO: Implement this method
-        return 0;
+        return dict.size();
     }
 
     /** Is this a word according to this dictionary? */
     public boolean isWord(String s) {
-        //TODO: Implement this method
-        return false;
-    }
+        String stringCheck = s.toLowerCase();
+        if (dict.contains(stringCheck)){
+            return true;
+        }
+        else{
+            return false;
+        }
 
-    
+    }
 }
+
